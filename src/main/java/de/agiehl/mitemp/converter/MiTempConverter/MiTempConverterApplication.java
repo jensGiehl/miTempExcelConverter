00b7e84f2d395e873681ac4176512897af869e56
Jens Giehl <mobil.jgiehl@gmail.com>
2021-12-01 12:28:27 +0100

    Init commit
    Project made with SpringBoot Starter
    
    Add gitignore from gitignore.io
    
    Add WebJar

---
+++ src/main/java/de/agiehl/mitemp/converter/MiTempConverter/MiTempConverterApplication.java
@@ -0,0 +1,13 @@
+package de.agiehl.mitemp.converter.MiTempConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiTempConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiTempConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/agiehl/mitemp/converter/MiTempConverter/MiTempConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.agiehl.mitemp.converter.MiTempConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiTempConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
